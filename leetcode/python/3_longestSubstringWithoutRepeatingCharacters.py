0aa39fbb722c4b0bcaa666a17013c2acae4dc9fc
Anirudh Vaish <47653244+avaish1409@users.noreply.github.com>
2020-10-07 21:41:54 +0530

    Create 3_longestSubstringWithoutRepeatingCharacters.py

---
+++ leetcode/python/3_longestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,26 @@
+# 3. Longest Substring Without Repeating Characters
+
+class Solution:
+    def isUnq(self,s):
+        for i in range(len(s)):
+            if s.count(s[i]) != 1:
+                return False
+        return True
+    
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        str_list = []
+        max_length = 0
+
+        for x in s:
+            if x in str_list:
+                str_list = str_list[str_list.index(x)+1:]
+
+            str_list.append(x)    
+            max_length = max(max_length, len(str_list))
+
+        return max_length
