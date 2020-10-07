6bcd29d7494f83390fe15c89112098d437059113
Anirudh Vaish <47653244+avaish1409@users.noreply.github.com>
2020-10-07 21:36:32 +0530

    Create 1_twoSum.py

---
+++ leetcode/python/1_twoSum.py
@@ -0,0 +1,10 @@
+[Two Sum Problem](https://leetcode.com/problems/two-sum/)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = []
+        for i in range(len(nums)-1):
+            if nums[i+1:].count(target-nums[i]) != 0:
+                res = [i, 1+i+nums[i+1:].index(target-nums[i])]
+                break
+        return res
